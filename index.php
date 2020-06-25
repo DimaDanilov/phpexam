d0dbdad34b2644adff76d3de6ef4bcda5db4e79e
Dmitry <dima.danilov.2012@mail.ru>
2020-06-25 10:14:27 +0300

    added admin login(test)

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db="phpforms_database";
+ 
+$a=mysqli_connect($host,$user,$password,$db);
+mysqli_select_db($a,$db);
+
+if(isset($_POST['username'])){
+    
+$uname=$_POST['username'];
+$password=$_POST['password'];
+    
+    $sql="select * from login_form where user='{$uname}'AND password='{$password}'";
+    $result=mysqli_query($a, $sql);
+    
+    if(mysqli_num_rows($result)!=0){
+        echo "You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +35,7 @@
     <title>Вход</title>
 </head>
 <body>
-    <form>
+    <form method="POST" action="#">
     <h1 class="form_h1">Вход для администратора</h1>
         <div class="form_container">
             <div class="form_field">
